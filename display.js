5d69a5137dcb1fe0c276abe57f6a0d3f25cac2d3
Stephens37 <97484915+Stephens37@users.noreply.github.com>
2024-04-28 10:41:49 -0500

    'Move comments in display.js to bottom'

--- display.js
+++ display.js
@@ -1,26 +1,7 @@
-/*
-take the matrix created in gameboard
-use that to make a 10x10 grid of squares
-
-const boardArr = Array.from({ length: 10 }, () => Array(10).fill(0))
-for each part of this array
-make a square
-
-need to connect the creation of this array with a grid created in css grid
-
-make the grid in css grid
-create loop that assigns square i , with i being the number, x and y coordinates
-assign each square a position a grid column and row position based upon their x and y coordinates
-make these squares children of the board class
-*/
+const gameboard = require('./gameboard.js')
 
 let playSqArr = []
 let compSqArr = []
-
-/*
-  create an array of coordinates without assigning them to a variable
-  loop through this array and assign each objects xCor and yCor as grid column/row values
-*/
 
 function assignPCor () {
   let x = 0
@@ -52,15 +33,49 @@ function assignCCor () {
 }
 assignCCor()
 
+const playerBoard = document.querySelector('#playerboard')
+const computerBoard = document.querySelector('#computerboard')
+
+function createPlayerSquares() {
+  for(let i = 0; i < 100; i++) {
+    let square = document.createElement('div')
+    square.setAttribute('class', 'playersq')
+    playerBoard.appendChild(square)
+  }
+}
+createPlayerSquares()
+
+function createComputerSquares() {
+  for(let i = 0; i < 100; i++) {
+    let square = document.createElement('div')
+    square.setAttribute('class', 'computersq')
+    square.coordinates = gameboard.coordinates[i]
+    computerBoard.appendChild(square)
+  }
+}
+createComputerSquares()
+
+
+
+
+
+
+
+
+
+
+
+/*
+  create an array of coordinates without assigning them to a variable
+  loop through this array and assign each objects xCor and yCor as grid column/row values
+*/
+
 /* the only question i have about not assigning a variable
 is how I will be able to assign colours, etc. to the squares
 
 perhaps later, i then find a match with the x and y coordinates,
 ex: if (grid-column: xCor && grid-row: yCor) then that grid-column and row is red
 */
-
-const playerBoard = document.querySelector('#playerboard')
-const computerBoard = document.querySelector('#computerboard')
 
 /*
   square is clicked
@@ -77,24 +92,6 @@ const computerBoard = document.querySelector('#computerboard')
   to create this I need to assign each square to a div
   I don't want 100 html elements so I want to create these in a loop
 */
-
-function createPlayerSquares() {
-  for(let i = 0; i < 100; i++) {
-    let square = document.createElement('div')
-    square.setAttribute('class', 'playersq')
-    playerBoard.appendChild(square)
-  }
-}
-createPlayerSquares()
-
-function createComputerSquares() {
-  for(let i = 0; i < 100; i++) {
-    let square = document.createElement('div')
-    square.setAttribute('class', 'computersq')
-    computerBoard.appendChild(square)
-  }
-}
-createComputerSquares()
 
 /*
   need to be able to retrieve a square based on its x and y coordinates
@@ -130,4 +127,3 @@ function compSquares () {
 
 compSquares()
 */
-
